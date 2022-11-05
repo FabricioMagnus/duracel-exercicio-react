df98ced178d983c625aeb30f556c755768e45f67
Fabricio Pompeo Magnus <fabricio.magnus@softkuka.com.br>
2022-11-04 23:59:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { Flex, SimpleGrid } from '@chakra-ui/react';
-import BoxCardList from './components/boxCardList';
+import { Button, Flex, SimpleGrid } from "@chakra-ui/react";
+import { useState } from "react";
+import BoxCardList from "./components/boxCardList";
 
 function App() {
-  return (
-    <Flex w={'100vw'} h={'100vh'}  justifyContent={"center"} px={10} py={10} alignItems={"center"}> 
-      <SimpleGrid>
-      <BoxCardList/>
-      </SimpleGrid>
-    </Flex>
-      
-    
-  );
+    const [numPilhas, setNumPilhas] = useState(1);
+
+    return (
+        <>
+            <Flex mt={14} h={"10vh"} w={"80vw"} margin={"auto"} borderBottomRadius={"30px"} bgColor={"gray"} mb={30} justifyContent={"space-evenly"} alignItems="center">
+                <Button onClick={() => setNumPilhas(numPilhas + 1)} h={"60px"} color="black" borderRadius={"16px"} cursor={"pointer"}>
+                    Adicionar Pilha
+                </Button>
+                <Button onClick={() => setNumPilhas(numPilhas - 1)} h={"60px"} color="black" borderRadius={"16px"} cursor={"pointer"}>
+                    Remover Pilha
+                </Button>
+            </Flex>
+            <Flex justifyContent={"center"} px={10} py={10} alignItems={"center"}>
+                <SimpleGrid columns={4} spacingY={40} spacingX={30}>
+                    <BoxCardList />
+                    <BoxCardList />
+                    <BoxCardList />
+                    <BoxCardList />
+                    <BoxCardList />
+                </SimpleGrid>
+            </Flex>
+        </>
+    );
 }
 
 export default App;
